93e835aa0f030f185a779b434fef2feb2b18567f
Proshanta15 <roy15-6997@diu.edu.bd>
2026-09-10 23:29:22 +0600

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { HomePage } from "./pages/HomePage";
+
 function App() {
   return (
     <>
-      <div className="ticks">Hello</div>
+      <div className="min-h-screen bg-base-200 transition-colors duration-300">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+export const Navbar = () => {
+    return (
+        <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 z-50">
+            <div className="max-w-7xl mx-auto">
+                <div className="navbar px-4 indicator"></div>
+            </div>
+        </div>
+    )
+}
