82f5aee04859af6f97efac43c20294488caf13b4
Proshanta Kumar Roy <Proshanta.ict@pakiza.com>
2026-09-10 15:59:11 +0600

    product store

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,21 +1,82 @@
 import { sql } from "../config/db.js";
 
 export const getProducts = async (req, res) => {
-    try {
-        const products = await sql `
+  try {
+    const products = await sql`
         SELECT * FROM products
         ORDER BY created_at DESC
         `;
-        res.status(200).json({success: true, data: products});
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const { name, image, price } = req.body;
+  if (!name || !image || !price) {
+    return res.status(400).json({
+      message: "Please provide name, image, and price for the product.",
+    });
+  }
+  try {
+    const newProduct = await sql`
+        INSERT INTO products (name, image, price)
+        VALUES (${name}, ${image}, ${price})
+        RETURNING *
+        `;
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await sql`
+        SELECT * FROM products WHERE id=${id}
+        `;
+    if (product.length === 0) {
+      return res.status(404).json({ message: "Product not found" });
     }
- }
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export const getProduct = async (req, res) => { }
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, image, price } = req.body;
 
-export const createProduct = async (req, res) => { }
+  try {
+    const updatedProduct = await sql`
+            UPDATE products
+            SET name = ${name}, image = ${image}, price = ${price}
+            WHERE id = ${id}
+            RETURNING *
+        `;
+    if (updatedProduct.length === 0) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ success: true, data: updatedProduct[0] });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export const updateProduct = async (req, res) => { }
-
-export const deleteProduct = async (req, res) => { }
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedProduct = await sql`
+        DELETE FROM products WHERE id=${id} RETURNING *
+        `;
+    if (deletedProduct.length === 0) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ success: true, data: deletedProduct[0] });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
