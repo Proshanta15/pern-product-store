c0524a472ef7cc0d24cf56aa3704fa8765f30f05
Proshanta15 <roy15-6997@diu.edu.bd>
2026-09-11 23:29:07 +0600

    frontend

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,13 +1,24 @@
+import { EditIcon, TrashIcon } from "lucide-react";
+import { Link } from 'react-router-dom';
 
 export const ProductCard = ({ product }) => {
     return (
         <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
-            <figure className="relative pt-[56.25%]">
-                <img src={product.image} alt={product.name} />
-            </figure>
+
+            <img className=" w-full h-full object-cover" src={product.image} alt={product.name} />
+
             <div className="card-body">
                 <h2 className="card-title text-lg font-semibold">{product.name}</h2>
                 <p className="text-2xl font-bold text-primary">${Number(product.price).toFixed(2)}</p>
+
+                <div className="card-actions justify-end mt-4">
+                    <Link to={`/product/${product.id}`} className="btn btn-sm btn-info btn-outline">
+                        <EditIcon className="size-4" />
+                    </Link>
+                    <button className="btn btn-sm btn-error btn-outline">
+                        <TrashIcon className="size-4" />
+                    </button>
+                </div>
             </div>
         </div>
     )
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ export const HomePage = () => {
     }, [fetchProducts]);
 
     return (
-        <main className="mx-auto px-4 py-8 max-w-6xl">
+        <main className="mx-auto px-4 py-8 max-w-7xl">
             <div className="flex justify-between items-center mb-8">
                 <button className="btn btn-primary">
                     <PlusCircleIcon className="size-5 mr-2" />Add Product
@@ -24,7 +24,7 @@ export const HomePage = () => {
                     <div className="loading loading-spinner loading-lg" />
                 </div>
             ) : (
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                     {products.map(product => (
                         <ProductCard key={product.id} product={product} />
                     ))}
