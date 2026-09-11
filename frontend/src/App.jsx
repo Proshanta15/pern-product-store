a0b7b707d43df1c3e101c97b5db9dfede2629d7b
Proshanta15 <roy15-6997@diu.edu.bd>
2026-09-11 16:19:30 +0600

    design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { HomePage } from "./pages/HomePage";
+import { useThemeStore } from "./store/useThemeStore";
 
 function App() {
+  const { theme } = useThemeStore();
   return (
     <>
-      <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
